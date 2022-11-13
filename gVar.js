e018a54a635bd2801fa338f1c961b18dd44c817d
sylintBundy <70914358+sylintBundy@users.noreply.github.com>
2022-11-12 19:44:46 -0600

    Update gVar.js

--- gVar.js
+++ gVar.js
@@ -3,15 +3,43 @@
 // The global variable for all data of each of the user's rooms.
 let db = [];
 let	dn = [];
+// If the browser doesn't support storage, turn this to true. Can be turn on manually for testing.
 let offline = false;
 
 // Check if the browser supports localStorage.
 if (typeof(Storage) !== "undefined") {
-	// If so, load the localStorage names and the JSON data for each room.
+	// If so, load the localStorage names and the JSON data for each room. Check if forced offline mode is on first.
+	if (!offline) {
+		if (localStorage.getItem("dataNames") != null) {
+			dn = JSON.parse(localStorage.getItem("dataNames"));
+		}
+		for (var name of dn) {
+			db.push(JSON.parse(localStorage.getItem(name)));
+		}
+	}
 } else {
 	// Otherwise, switch to offline mode and notify the user.
 	offline = true;
 	window.alert("Oops!\nIt looks like your browser version doesn't support localStorage, which we need to save your rooms.\nFeel free to use SquareRooom in temporary mode.");
+}
+
+function iSave() {
+	if (!offline) {
+		localStorage.setItem("dataNames", JSON.stringify(dn));
+		for (var room of db) {
+			localStorage.setItem(room.n, JSON.stringify(room));
+		}
+	}
+	else console.log("Saving failed because the page is in offline mode.");
+}
+
+function checkRoomExists(name) {
+	for (var room of dn) {
+		if (room == name) {
+			return true;
+		}
+	}
+	return false;
 }
 
 // Table of colors to use
@@ -31,20 +59,3 @@ const colors = {
 	Magenta: "330, 080, 080, Magenta",
 	White: "000, 000, 090, White"
 };
-
-class Room {
-	constructor(name) {
-		this.name = name;
-	}
-}
-
-class Object {
-	constructor(shape, name) {
-		this.shape = shape;
-		this.name = name;
-		this.posx = 0;
-		this.posy = 0;
-		this.scalex = 1;
-		this.scaley = 1;
-	}
-}
