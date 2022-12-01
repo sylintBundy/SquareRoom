c09fc4189f0607c91a58871d69ec6af882d60c37
sylintBundy <70914358+sylintBundy@users.noreply.github.com>
2022-12-01 14:07:48 -0600

    Add files via upload

---
+++ iframe/data.js
@@ -0,0 +1,23 @@
+// Handles the saving and retrieval of localStorage, so as to persist it between the two domains.
+
+function messageHandler(event) {
+	const {action, key, value} = event.data;
+	if (action == 'save') {
+		window.localStorage.setItem(key, JSON.stringify(value));
+	}
+	else if (action == 'get') {
+		if (window.localStorage.getItem(key) != null) {
+			event.source.postMessage({action: 'returnData', key: key, value: JSON.parse(window.localStorage.getItem(key))}, '*');
+		}
+		else event.source.postMessage({action: 'returnData', key: key, value: null}, '*');
+	}
+	else if (action == 'delete') {
+		if (window.localStorage.getItem(key) != null) {
+			window.localStorage.removeItem(key);
+		}
+		window.localStorage.removeItem(key);
+	}
+	else console.error(action + " is not a valid action to iframe.");
+}
+
+window.addEventListener("message", messageHandler, false);
