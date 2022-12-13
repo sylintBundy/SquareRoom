1865c7dbb68a1b8bba2b6bce63bec395570a4472
sylintBundy <70914358+sylintBundy@users.noreply.github.com>
2022-12-12 19:26:14 -0600

    Update editpage.js

--- editpage/editpage.js
+++ editpage/editpage.js
@@ -1,27 +1,27 @@
 // Handles the interactive fields above the canvas
 
+pageNumber = 2;
+
 $(function() {
 	pageSetup();
 	$('.modeSwitch').on('click', changeMode);
+	$('#returnMenu').on('click', returnToMenu);
+	$('#saveButton').on('click', iSave);
 })
 
 function pageSetup() {
+	var parameters = new URLSearchParams(window.location.search);
+	editingPage = parameters.get("editingRoom");
+	if (editingPage == null) {
+		offline = true;
+	}
 	iLoad();
 	refreshOptions();
 	document.getElementById('addItem').addEventListener('click', addElement);
+	document.getElementById('deleteItem').addEventListener('click', removeElement);
 	var propertyFields = document.querySelectorAll('.propField');
 	for (var field of propertyFields) {
 		field.addEventListener('change', changeElement);
-	}
-}
-
-function loadRoom() {
-	for (var room of databaseRooms) {
-		if (room.editing == true) {
-			room.editing = false;
-			rm = room;
-			break;
-		}
 	}
 }
 
@@ -69,7 +69,7 @@ function changeMode(t) {
 		}
 		mode = t.target.value;
 		if (mode == 0) {
-			document.getElementById('manipMenu').innerHTML = "<label>Add an Object...</label><br><select id='objectMenu'></select><button id='addItem'>Add</button><br><br><div class='editRow'><div id='elName'><label>Object Name</label><br><input id='nameField' class='propField' value='Object Name'></div><div id='colors'><label>Color</label><br><select id='colorMenu' class='propField'></select></div><div><label>Rotation</label><br><input id='rotationField' class='propField' type='number' value=0></div></div><br><br><div class='editRow'><div><label>X Position (in)</label><br><input id='xPosField' class='propField' type='number' value=0></div><div><label>X Scale</label><br><input id='xScaleField' class='propField' type='number' value=1></div><div><label>Y Position (in)</label><br><input id='yPosField' class='propField' type='number' value=0></div><div><label>Y Scale</label><br><input id='yScaleField' class='propField' type='number' value=1></div></div>"
+			document.getElementById('manipMenu').innerHTML = "<div id='topRow'><div id='ojMenu'><label>Add an Object...</label><br><select id='objectMenu'></select></div><div id='addBtn'><br><button id='addItem'>Add</button></div><div id='rmvBtn'><br><button id='deleteItem'>Remove</button></div></div><br><br><div class='editRow'><div id='elName'><label>Object Name</label><br><input id='nameField' class='propField' value='Object Name'></div><div id='colors'><label>Color</label><br><select id='colorMenu' class='propField'></select></div><div><label>Rotation</label><br><input id='rotationField' class='propField' type='number' value=0></div></div><br><br><div class='editRow'><div><label>X Position (in)</label><br><input id='xPosField' class='propField' type='number' value=0></div><div><label>X Size (in)</label><br><input id='xScaleField' class='propField' type='number' value=1></div><div><label>Y Position (in)</label><br><input id='yPosField' class='propField' type='number' value=0></div><div><label>Y Size (in)</label><br><input id='yScaleField' class='propField' type='number' value=1></div></div>";
 			refreshOptions();
 			document.getElementById('elementMode').style = "border-bottom-style: solid";
 			document.getElementById('addItem').addEventListener('click', addElement);
@@ -80,10 +80,9 @@ function changeMode(t) {
 			}
 		}
 		else if (mode == 1) {
-			document.getElementById('manipMenu').innerHTML = "<div class='editRow3'><div id='roomWidth'><label>Room Width (in)</label><br><input id='widthField' class='roomField' type='number'></div><div id='roomDepth'><label>Room Depth (in)</label><br><input id='depthField' class='roomField' type='number'></div><div id='doorWidth'><label>Doorframe Width (in)</label><br><input id='doorField' class='roomField' type='number'></div></div>"
+			document.getElementById('manipMenu').innerHTML = "<div class='editRow2'><div id='roomWidth'><label>Room Width (in)</label><br><input id='widthField' class='roomField' type='number'></div><div id='roomDepth'><label>Room Depth (in)</label><br><input id='depthField' class='roomField' type='number'></div></div>";
 			document.getElementById('widthField').value = rm.s.w / 5;
 			document.getElementById('depthField').value = rm.s.h / 5;
-			document.getElementById('doorField').value = rm.s.d / 5;
 			document.getElementById('roomMode').style = "border-bottom-style: solid";
 			var propertyFields = document.querySelectorAll('.roomField');
 			for (var field of propertyFields) {
@@ -92,7 +91,7 @@ function changeMode(t) {
 			selElement = -1;
 		}
 		else {
-			document.getElementById('manipMenu').innerHTML = "<label>Click on two points in the canvas to measure the length!</label><div id='infoRow'><div id='toolDistance'><b>Measuring Length:</b></div><div id='toolWidth'>Measuring Width:</div><div id='toolHeight'>Measuring Depth:</div></div>";
+			document.getElementById('manipMenu').innerHTML = "<label>Click on two points in the canvas to measure the length!</label><div id='infoRow'><div id='toolDistance'><b>Measuring Length:</b></div></div>";
 			document.getElementById('measureMode').style = "border-bottom-style: solid";
 		}
 	}
@@ -100,16 +99,25 @@ function changeMode(t) {
 
 function addElement() {
 	if (document.getElementById('objectMenu').value != null) {
+		var newElementShape = document.getElementById('objectMenu').value;
+		var newShapeData;
+		for (var element of elements) {
+			if (element[0] == newElementShape) {
+				newShapeData = element;
+				break;
+			}
+		}
 		var newElement = {
 			n: document.getElementById('nameField').value,
-			s: document.getElementById('objectMenu').value,
-			px: document.getElementById('xPosField').value * 5,
-			py: document.getElementById('yPosField').value * 5,
-			sx: document.getElementById('xScaleField').value,
-			sy: document.getElementById('yScaleField').value,
-			r: document.getElementById('rotationField').value,
+			s: newShapeData[0],
+			px: parseFloat(document.getElementById('xPosField').value) * 5,
+			py: parseFloat(document.getElementById('yPosField').value) * 5,
+			sx: newShapeData[1],
+			sy: newShapeData[2],
+			r: parseFloat(document.getElementById('rotationField').value),
 			c: document.getElementById('colorMenu').value
 		};
+		enforceBounds(newElement);
 	}
 	rm.e.push(newElement);
 }
@@ -120,15 +128,22 @@ function changeElement() {
 		element.n = document.getElementById('nameField').value;
 		element.px = parseFloat(document.getElementById('xPosField').value) * 5;
 		element.py = parseFloat(document.getElementById('yPosField').value) * 5;
-		element.sx = parseFloat(document.getElementById('xScaleField').value);
-		element.sy = parseFloat(document.getElementById('yScaleField').value);
+		element.sx = parseFloat(document.getElementById('xScaleField').value) * 5;
+		element.sy = parseFloat(document.getElementById('yScaleField').value) * 5;
 		element.r = parseFloat(document.getElementById('rotationField').value);
 		element.c = document.getElementById('colorMenu').value;
 	}
 }
 
 function removeElement() {
-	rm.e.splice(selElement, 1);
-	selElement = -1;
-	updateFields(selElement);
+	if (selElement != -1) {
+		rm.e.splice(selElement, 1);
+		selElement = -1;
+		updateFields(selElement);
+	}
+}
+
+function returnToMenu() {
+	iSave();
+	location.href = "../index.html";
 }
